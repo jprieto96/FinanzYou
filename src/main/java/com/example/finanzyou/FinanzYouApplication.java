50440283d44078193aec798935939550705246b0
Jose Prieto <joseprieto9657@gmail.com>
2022-02-18 17:31:47 +0100

    Initial commit

---
+++ src/main/java/com/example/finanzyou/FinanzYouApplication.java
@@ -0,0 +1,13 @@
+package com.example.finanzyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanzYouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finanzyou/FinanzYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finanzyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
