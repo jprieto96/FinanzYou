5e4b09aa3e9ed14f4c21154e7971888e9abbd465
Jose Prieto <joseprieto9657@gmail.com>
2022-02-20 12:55:04 +0100

    added sector to details portfolio

--- src/main/java/com/example/finanzyou/model/client/DetailsPortfolio.java
+++ src/main/java/com/example/finanzyou/model/client/DetailsPortfolio.java
@@ -5,13 +5,15 @@ import lombok.Data;
 @Data
 public class DetailsPortfolio {
 
+    private String sector;
     private int quantity;
     private double buyPrice;
 
     public DetailsPortfolio() {}
 
-    public DetailsPortfolio(int quantity, double buyPrice) {
+    public DetailsPortfolio(int quantity, double buyPrice, String sector) {
         this.quantity = quantity;
         this.buyPrice = buyPrice;
+        this.sector = sector;
     }
 }
