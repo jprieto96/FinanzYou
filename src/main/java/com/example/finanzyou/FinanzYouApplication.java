b8f8acbbd47d20b150de9dacae767ad954c0e06f
Jorge del Prado Prieto <jorge.delprado@stratesys-ts.com>
2022-02-22 09:22:21 +0100

    Port 8081 added

--- src/main/java/com/example/finanzyou/FinanzYouApplication.java
+++ src/main/java/com/example/finanzyou/FinanzYouApplication.java
@@ -20,7 +20,7 @@ public class FinanzYouApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https://finanzyou-front.herokuapp.com/", "http://localhost:8080"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://finanzyou-front.herokuapp.com/", "http://localhost:8080", "http://localhost:8081"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
